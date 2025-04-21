9099dacb9bb919feba9810d0cf7499f183059e53
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2025-04-21 21:48:06 +0300

    Remove redundant config lines

--- config.sh
+++ config.sh
@@ -8,8 +8,5 @@ c_remote_sessions_file_path="/tmp/tses-remote-sessions"
 # local log path
 c_log_file_path="${HOME}/.local/share/tx/tx.log"
 
-# port that will be forwared to remote hosts and used to communicate to the server
-c_ctrl_port="12399"
-
 # other ports that should be forwarded, for example:
 c_forward_ports+=("12398")
