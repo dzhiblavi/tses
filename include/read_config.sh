c62feddacd7e2e8f22234b57d7c352a54b2cf715
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2025-04-21 20:50:05 +0300

    Make configuration optional

--- include/read_config.sh
+++ include/read_config.sh
@@ -2,10 +2,7 @@ c_ctrl_port="12399"
 declare -A c_host_alias_to_hostname=()
 declare -a c_forward_ports=($c_ctrl_port)
 
-. "${HOME}/.config/tses/config.sh" || {
-    echo "cmd=read_config status=error reason='no configuration found at ~/.config/tses/config.sh'"
-    exit 1
-}
+. "${HOME}/.config/tses/config.sh" || true
 
 declare -a c_host_aliases=()
 declare -A c_hostname_to_host_alias=()
